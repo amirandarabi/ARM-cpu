9ae41f9e8179fcc2adbfe8cb836a83acb3d926aa
amirandarabi <andarabi.amirhosein@gmail.com>
2018-12-17 22:21:20 +0330

    edit adder.v

--- adder.v
+++ adder.v
@@ -1,6 +1,7 @@
 module adder ( cin , a, b , s, cout);
-input [31:0] cin, b, a ;
-output [31:0] cout,s;
+parameter n = 31;
+input [n:0] cin, b, a ;
+output [n:0] cout,s;
 
 assign s = a ^ b ^cin ;
 assign cout =(a & b)|(a & cin)|(b &cin);
