caa3b51ac72b93a316429d795ddb073cd36e60c7
amirandarabi <andarabi.amirhosein@gmail.com>
2018-12-28 12:59:16 +0330

    Update MUX.v

--- MUX.v
+++ MUX.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module Mux
   #( 
   parameter n = 64
@@ -12,20 +11,5 @@ module Mux
   );
   
   assign muxOut = selector ? in2 : in1; 
-  
-=======
-module Mux( s, a, b, f);
-	parameter n = 5;
-	input s;
-	input [4:0]a,b;
-	output reg[4:0]f;
-	
-	always
-	begin
-		case(s)
-       		0:f=a;
-		1:f=b;
-		endcase
-	end
->>>>>>> 580371176b7c4d2e5050d4df0445b3a909052ac6
+
 endmodule
